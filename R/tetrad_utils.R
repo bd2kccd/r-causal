18af4705bdd3c6a1382603e5481d6fb373a2562c
chirayukong <chirayu.kong@gmail.com>
2016-06-27 12:34:52 -0400

    Fixed Temporal again

--- R/tetrad_utils.R
+++ R/tetrad_utils.R
@@ -240,8 +240,9 @@ priorKnowledge <- function(forbiddirect = NULL, requiredirect = NULL, addtempora
             for(j in 1:length(temporal)){
                 node <- temporal[j]
                 node <- gsub(" ", ".", node)
-                #name <- .jnew("java/lang/String", node)
-                prior$addToTier((i-1), node)
+                cat("temporal node: ", node, "\n")
+                name <- .jnew("java/lang/String", node)
+                prior$addToTier((i-1), name)
                 #.jcall(prior, "IV", "addToTier", (i-1), name)
             }
         }
