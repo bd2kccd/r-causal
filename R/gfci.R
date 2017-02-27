520c7e38d719f206834cac3a5eaef50f2e5ea954
chirayukong <chirayu.kong@gmail.com>
2017-02-27 15:07:55 -0500

    Fixed out-dated MaxInDegree parameters in Gfci

--- R/gfci.R
+++ R/gfci.R
@@ -1,4 +1,4 @@
-gfci <- function(df, penaltydiscount = 2.0, maxInDegree = 3, maxPathLength = -1, significance = 0.05,
+gfci <- function(df, penaltydiscount = 2.0, maxDegree = 3, maxPathLength = -1, significance = 0.05,
     completeRuleSetUsed = FALSE, faithfulnessAssumed = TRUE, verbose = FALSE, java.parameters = NULL, 
     priorKnowledge = NULL){
     
@@ -28,7 +28,7 @@ gfci <- function(df, penaltydiscount = 2.0, maxInDegree = 3, maxPathLength = -1,
 
     # Initiate GFCI
     gfci_instance <- .jnew("edu/cmu/tetrad/search/GFci", indTest, score)
-    .jcall(gfci_instance, "V", "setMaxIndegree", as.integer(maxInDegree))
+    .jcall(gfci_instance, "V", "setMaxDegree", as.integer(maxDegree))
     .jcall(gfci_instance, "V", "setMaxPathLength", as.integer(maxPathLength))
     .jcall(gfci_instance, "V", "setCompleteRuleSetUsed", completeRuleSetUsed)
     .jcall(gfci_instance, "V", "setFaithfulnessAssumed", faithfulnessAssumed)
@@ -39,7 +39,7 @@ gfci <- function(df, penaltydiscount = 2.0, maxInDegree = 3, maxPathLength = -1,
     }
 
 	params <- c(params, penaltydiscount = as.double(penaltydiscount))
-    params <- c(params, maxInDegree = as.integer(maxInDegree))
+    params <- c(params, maxDegree = as.integer(maxDegree))
     params <- c(params, maxPathLength = as.integer(maxPathLength))
     params <- c(params, significance = significance)
     params <- c(params, completeRuleSetUsed = as.logical(completeRuleSetUsed))
@@ -53,7 +53,7 @@ gfci <- function(df, penaltydiscount = 2.0, maxInDegree = 3, maxPathLength = -1,
 
     cat("Graph Parameters:\n")
     cat("penaltydiscount = ", penaltydiscount,"\n")
-    cat("maxInDegree = ", as.integer(maxInDegree),"\n")
+    cat("maxDegree = ", as.integer(maxDegree),"\n")
     cat("maxPathLength = ", as.integer(maxPathLength),"\n")
     cat("significance = ", significance,"\n")
     cat("completeRuleSetUsed = ", completeRuleSetUsed,"\n")
